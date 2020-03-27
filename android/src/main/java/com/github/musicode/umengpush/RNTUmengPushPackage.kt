0d41bcee6aabe6b452a11c69d02c37bfc448936a
musicode <musicode@qq.com>
2020-03-27 15:17:34 +0800

    bump 0.2.2

--- android/src/main/java/com/github/musicode/umengpush/RNTUmengPushPackage.kt
+++ android/src/main/java/com/github/musicode/umengpush/RNTUmengPushPackage.kt
@@ -1,7 +1,5 @@
 package com.github.musicode.umengpush
 
-import java.util.Arrays
-
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
@@ -10,7 +8,7 @@ import com.facebook.react.uimanager.ViewManager
 class RNTUmengPushPackage : ReactPackage {
 
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return Arrays.asList<NativeModule>(RNTUmengPushModule(reactContext))
+        return listOf<NativeModule>(RNTUmengPushModule(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
